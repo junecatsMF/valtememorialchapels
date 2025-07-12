420d56b6bd669a8474f6cde35616995a44e4160b
june-cats <junerodrigo.cataquez@sansan.com>
2025-07-12 17:16:30 +0800

    Initial commit for base code

---
+++ src/main/java/com/valte/valtemc/ValtemcApplication.java
@@ -0,0 +1,13 @@
+package com.valte.valtemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValtemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValtemcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valte/valtemc/ValtemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valte.valtemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValtemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
